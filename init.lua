f966674725c90de6bd6950cac5f4082acdd477bf
sjd23dev <SJD23Dev@proton.me>
2025-04-05 00:05:07 -0500

    Add new settings and other configurations

--- init.lua
+++ init.lua
@@ -1,10 +1,42 @@
+-- Set space as leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = "a"
+vim.opt.cursorline = false
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.cmdheight = 1
+vim.opt.showmode = false
+
+vim.opt.mouse = ""
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+
+vim.opt.backspace = { "indent", "eol", "start" }
+
+vim.opt.completeopt = { "menuone", "noselect" }
 
 require("config.keymaps")
 
